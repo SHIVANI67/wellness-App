0165b70dc03e9a7ed38046a14606b7bc92903593
SHIVANI67 <shivanibit25@gmail.com>
2020-12-30 09:11:58 +0530

    Initial commit

---
+++ app/src/main/java/com/e/welnessapp/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.e.welnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    EditText etMobileNum;
+    Button getOtpButton;
+    String phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        etMobileNum = findViewById(R.id.et_enter_phone);
+        getOtpButton = findViewById(R.id.get_otp_button);
+
+
+        if(etMobileNum.getText() != null) {
+            if(etMobileNum.length() == 10) {
+                phoneNumber = etMobileNum.getText().toString();
+            } else {
+                Toast.makeText(this, "Phone number should be of 10 digits", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Cannot get otp without a phone number", Toast.LENGTH_SHORT).show();
+        }
+
+        getOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(phoneNumber.length() != null) {
+                    
+                }
+            }
+        });
+    }
+}
